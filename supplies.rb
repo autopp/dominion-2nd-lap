8d92b1f9213b5c9bf024188666f87888d20ebf37
autopp <autopp.inc@gmail.com>
2020-03-09 23:20:17 +0900

    feat: add double supplies

--- supplies.rb
+++ supplies.rb
@@ -3,30 +3,6 @@ require_relative 'tactic'
 class Supplies < Tactic
   SUPPLIES = :supplies
 
-  def title
-    '銀貨・Supplies で4ターン目までに……'
-  end
-
-  def gen_decks
-    with_combination_of_estates(12) do |factory, other_indices|
-      other_indices.permutation(2).map do |(silver, supplies)|
-        factory.new_deck do |deck|
-          deck[silver] = SILVER
-          deck[supplies] = SUPPLIES
-        end
-      end
-    end
-  end
-
-  def split_to_hands(deck)
-    case deck.find_index(SUPPLIES)
-    when 0...5
-      [deck[0...5].sort!, deck[5...11].sort!]
-    else
-      [deck[0...5].sort!, deck[5...10].sort!]
-    end
-  end
-
   def simulate_turn(hand)
     { coin: sum_of_coin(hand, SUPPLIES => -> { 1 }) }
   end
@@ -52,4 +28,60 @@ class Supplies < Tactic
   end
 end
 
-Supplies.new.report
+class SuppliesWithSilver < Supplies
+  def title
+    '銀貨・Supplies で4ターン目までに……'
+  end
+
+  def gen_decks
+    with_combination_of_estates(12) do |factory, other_indices|
+      other_indices.permutation(2).map do |(silver, supplies)|
+        factory.new_deck do |deck|
+          deck[silver] = SILVER
+          deck[supplies] = SUPPLIES
+        end
+      end
+    end
+  end
+
+  def split_to_hands(deck)
+    case deck.find_index(SUPPLIES)
+    when 0...5
+      [deck[0...5].sort!, deck[5...11].sort!]
+    else
+      [deck[0...5].sort!, deck[5...10].sort!]
+    end
+  end
+end
+
+class DoubleSupplies < Supplies
+  def title
+    'Supplies・Supplies で4ターン目までに……'
+  end
+
+  def gen_decks
+    with_combination_of_estates(12) do |factory, other_indices|
+      other_indices.permutation(2).map do |(supplies1, supplies2)|
+        factory.new_deck do |deck|
+          deck[supplies1] = SUPPLIES
+          deck[supplies2] = SUPPLIES
+        end
+      end
+    end
+  end
+
+  def split_to_hands(deck)
+    case deck[0...5].count(SUPPLIES)
+    when 2
+      [deck[0...5].sort!, deck[5...12].sort!]
+    when 1
+      [deck[0...5].sort!, deck[5...11].sort!]
+    else
+      [deck[0...5].sort!, deck[5...10].sort!]
+    end
+  end
+end
+
+SuppliesWithSilver.new.report
+puts
+DoubleSupplies.new.report
