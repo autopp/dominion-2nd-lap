81880ab85db1be89b02610c40c14bbaa20dec828
autopp <autopp.inc@gmail.com>
2020-05-15 22:12:37 +0900

    feat: add potion with storeroom

--- potion.rb
+++ potion.rb
@@ -93,6 +93,85 @@ class PotionWithDraw < Potion
   end
 end
 
+class PotionWithStoreroom < Potion
+  def title
+    "ポーション・物置で3T目の物置は#{num_of_discard}枚捨てる場合、デッキ3周目までに……"
+  end
+
+  def gen_decks
+    with_combination_of_estates(12) do |factory, other_indices|
+      other_indices.permutation(2).map do |(potion, action)|
+        factory.new_deck do |deck|
+          deck[potion] = POTION
+          deck[action] = ACTION
+        end
+      end
+    end
+  end
+
+  def split_to_hands(deck)
+    split_by_draw_action(deck, num_of_discard)
+  end
+
+  def simulate_turn(hand)
+    if hand.include?(ACTION)
+      { coin: hand.count(COPPER) + hand.count(ESTATE), potion: hand.include?(POTION) }
+    else
+      { coin: sum_of_coin(hand), potion: hand.include?(POTION) }
+    end
+  end
+
+  def simulate(deck)
+    hand3, hand4 = deck
+    t3 = simulate_turn(hand3)
+
+    if hand3.size > 7
+      {
+        potion_2: with_potion?(t3, 2),
+        potion_3: with_potion?(t3, 3)
+      }
+    else
+      t4 = simulate_turn(hand4)
+
+      {
+        potion_2: any_with_potion?(t3, t4, 2),
+        potion_3: any_with_potion?(t3, t4, 3)
+      }
+    end
+  end
+
+  def topics
+    {
+      potion_2: '2金 + ポーションを出せる確率',
+      potion_3: '3金 + ポーションを出せる確率'
+    }
+  end
+end
+
+class PotionWithStoreroom2 < PotionWithStoreroom
+  def num_of_discard
+    2
+  end
+end
+
+class PotionWithStoreroom3 < PotionWithStoreroom
+  def num_of_discard
+    3
+  end
+end
+
+class PotionWithStoreroom4 < PotionWithStoreroom
+  def num_of_discard
+    4
+  end
+end
+
 PotionWithSilver.new.report
 puts
 PotionWithDraw.new.report
+puts
+PotionWithStoreroom2.new.report
+puts
+PotionWithStoreroom3.new.report
+puts
+PotionWithStoreroom4.new.report
